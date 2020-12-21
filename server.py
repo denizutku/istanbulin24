2b1c4b670cfd47041353e10f739cc6818ce166d5
denizutku <beydogandeniz@gmail.com>
2020-12-21 12:35:14 +0300

    frontend configured and base pages created.

--- server.py
+++ server.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
+@app.route("/routes")
+def routes():
+    return render_template("routes.html")
+
+@app.route("/newroute")
+def newroute():
+    return render_template("new_route.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080,debug=True)
