d0f3bb321b4eac834d6ff8d6cf3759acac071bf7
Deniz <beydogandeniz@gmail.com>
2021-01-14 14:57:07 +0300

    created user, route, activity tables

--- dbinit.py
+++ dbinit.py
@@ -5,22 +5,50 @@ import psycopg2 as dbapi2
 
 
 INIT_STATEMENTS = [
+    """DROP TABLE IF EXISTS users cascade""",
+    """DROP TABLE IF EXISTS routes cascade""",
+    """DROP TABLE IF EXISTS activities cascade""",
+
         """CREATE TABLE IF NOT EXISTS users 
     (
-        user_id SERIAL NOT NULL PRIMARY KEY,
-        user_name VARCHAR(15) UNIQUE NOT NULL,
-        email VARCHAR (50) NOT NULL,
-        password VARCHAR (50) NOT NULL,
-        name VARCHAR (50) NOT NULL,
-        surname VARCHAR (50) NOT NULL,
-        phone VARCHAR (15) NOT NULL,
-        gender VARCHAR (1) NOT NULL,
-        address VARCHAR (250) NOT NULL,
-        last_login TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        register_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        is_admin BOOLEAN NOT NULL DEFAULT FALSE
+        id SERIAL NOT NULL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR (255) NOT NULL,
+        name VARCHAR (255) NOT NULL,
+        surname VARCHAR (255) NOT NULL,
+        email VARCHAR (255) NOT NULL
 
     )""",
+
+    """CREATE TABLE IF NOT EXISTS routes
+    (
+        id SERIAL NOT NULL PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR (255) NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES userS(id) ON UPDATE CASCADE ON DELETE CASCADE
+
+    )""",
+
+    """CREATE TABLE IF NOT EXISTS activities
+    (
+        id SERIAL NOT NULL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR (255) NOT NULL,
+        img_url VARCHAR (255) NOT NULL
+
+    )""",
+
+    # """INSERT INTO routes(id, username, password, name, surname, email) VALUES
+    # (
+    #     0,
+    #     'deniz',
+    #     'deniz',
+    #     'deniz',
+    #     'deniz',
+    #     'deniz'
+
+    # )""",
 ]
 
 
