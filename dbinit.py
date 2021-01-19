ce88ea5428df4ed8dfb51e2cef262a613bb7a4a1
Deniz <beydogandeniz@gmail.com>
2021-01-19 14:12:26 +0300

    activity description length fixed

--- dbinit.py
+++ dbinit.py
@@ -40,7 +40,7 @@ INIT_STATEMENTS = [
     (
         id SERIAL NOT NULL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
-        description VARCHAR (255) NOT NULL
+        description VARCHAR NOT NULL
 
     )""",
 
